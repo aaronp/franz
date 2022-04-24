cc217839d1f4feb4076d343c60256ff3c5f03773
Aaron Pritzlaff <aaronpritzlaff@nimbleapproach.com>
2022-04-24 20:10:40 +0100

    async

--- startLocalKafkaIntel.sh
+++ startLocalKafkaIntel.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker run --rm -p 2181:2181 -p 3030:3030 -p 8081-8083:8081-8083 \
            -p 9581-9585:9581-9585 -p 9092:9092 -e ADV_HOST=$(ipconfig getifaddr en0) \
-           lensesio/fast-data-dev
+           lensesio/fast-data-dev &
